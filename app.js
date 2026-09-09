26d919017db8b0c040e7412b433cd1d229edb788
Vivekananda <22h51a7314@cmrcet.ac.in>
2026-09-09 19:09:53 +0530

    Release version 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({
         message: "Hello from Node.js CI/CD!",
-        version: "1.0.0"
+        version: "2.0.0"
     });
 });
 
