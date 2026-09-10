6e182771e52f5a84a8226aac9de986a65c47c436
Vivekananda <22h51a7314@cmrcet.ac.in>
2026-09-10 19:44:40 +0530

    Added version 5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({
         message: "Hello from Node.js CI/CD!",
-        version: "3.0.0"
+        version: "5.0.0"
     });
 });
 
