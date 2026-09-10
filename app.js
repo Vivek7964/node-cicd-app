fe0e5ee38f38e61feb35e557f95900d0df30462f
Vivekananda <22h51a7314@cmrcet.ac.in>
2026-09-11 00:16:46 +0530

    Finale 0.0.1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({
         message: "Hello from Node.js CI/CD!",
-        version: "final.0.0"
+        version: "finale.0.1"
     });
 });
 
