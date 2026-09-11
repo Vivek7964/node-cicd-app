92a7ab5992c57ec1b51797709d117cb6de12af6f
Vivekananda <22h51a7314@cmrcet.ac.in>
2026-09-11 23:10:22 +0530

    Testing jenkins pipeline

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({
         message: "Hello from Node.js CI/CD!",
-        version: "ftestingg.teadsfasdsdfadfamp0.3"
+        version: "ftesdfatingg.teadsfasdsdfadfamp0.3"
     });
 });
 
