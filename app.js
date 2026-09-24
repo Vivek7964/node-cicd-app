9e09065356c187070da72aa5f8aa5f3576f822b2
Vivekananda <22h51a7314@cmrcet.ac.in>
2026-09-24 17:56:36 +0530

    Add Prometheus monitoring for Node.js application

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
 const express = require("express");
+const client = require("@prometheus-io/client");
 
 const app = express();
 
+client.collectDefaultMetrics();
+
+const httpRequestsTotal = new client.Counter({
+    name: "nodejs_http_requests_total",
+    help: "Total number of HTTP requests",
+    labelNames: ["method", "route", "status_code"]
+});
+
+app.use((req, res, next) => {
+
+    res.on("finish", () => {
+
+        httpRequestsTotal.inc({
+            method: req.method,
+            route: req.route?.path || req.path,
+            status_code: res.statusCode
+        });
+
+    });
+
+    next();
+});
+
 app.get("/", (req, res) => {
+
     res.json({
         message: "Hello from Node.js CI/CD!",
-        version: "1.0.4"
+        version: "1.0.0"
     });
+
 });
 
 app.get("/health", (req, res) => {
+
     res.status(200).json({
         status: "UP"
     });
+
+});
+
+app.get("/metrics", async (req, res) => {
+
+    res.set(
+        "Content-Type",
+        client.register.contentType
+    );
+
+    res.end(
+        await client.register.metrics()
+    );
+
 });
 
 module.exports = app;
